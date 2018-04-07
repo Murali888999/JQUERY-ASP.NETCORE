e01991b0b76cf6dbea6f1da4d535ab9f85e92c4f
Murali888999 <mohanggk24@gmail.com>
2018-04-07 20:19:08 +0530

    JQUERYandINTRODUCTION ASP.NETCORE

---
+++ ASPdotNETCore/AspdotNetCoreMVC/AspdotNetCoreMVC/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspdotNetCoreMVC.Controllers
+{
+    public class Homecontroller
+    {
+        public string Index()
+        {
+            return "hellooooo...this message from Homecontroller";
+        }
+    }
+}
